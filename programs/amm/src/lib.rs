f9e72cfdaa4a9e6099bbf59f9207b518146905e9
José Santos <josesantos@MBP-de-Jose-2.lan>
2025-01-27 21:54:26 +0000

    initial commit

---
+++ programs/amm/src/lib.rs
@@ -0,0 +1,57 @@
+use anchor_lang::prelude::*;
+
+mod instructions;
+use instructions::*;
+mod errors;
+mod helpers;
+mod state;
+
+declare_id!("HGitkDvzY7TVWCPPEfTMeGRKpSBu7b4MmerrPpr85q4e");
+
+#[program]
+pub mod amm {
+    use super::*;
+
+    // Ensures initialization of config, LP mint account, vault_x and vault_y accounts, maker_ata_x, maker_ata_y and maker_ata_lp accounts
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        seed: u64,
+        fee: u16,
+        amount_x: u64,
+        amount_y: u64,
+    ) -> Result<()> {
+        ctx.accounts.init_config(seed, fee, ctx.bumps.config, ctx.bumps.mint_lp)?;
+        Ok(())
+    }
+
+    // Add liquidity to mint LP tokens
+    pub fn deposit(
+        ctx: Context<Deposit>,
+        amount: u64, // amount of LP token to claim
+        max_x: u64,  // max amount of X we are willing to deposit
+        max_y: u64,  // max amount of Y we are willing to deposit
+        expiration: i64,
+    ) -> Result<()> {
+        ctx.accounts.deposit(amount, max_x, max_y, expiration)?;
+        Ok(())
+    }
+
+
+// // Burn LP tokens to withdraw liquidity
+// pub fn withdraw(ctx: Context<Withdraw>, amount: u64, min_x: u64, min_y: u64) -> Result<()> {
+//     // burn_lp_token(amount)?;
+//     // withdraw_tokens(amount)
+//     Ok(())
+// }
+
+// pub fn swap(
+//     ctx: Context<Swap>,
+//     amount: u64,
+//     min_receive: u64,
+//     is_x: bool, /*, expiration: i64 */
+// ) -> Result<()> {
+//     // deposit_token()?;
+//     // withdraw_token()
+//     Ok(())
+// }
+}
